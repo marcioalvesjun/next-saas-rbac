b47a283096cb648fa7acc1446873297ef71b6214
Marcio Alves <marcioalvesjun@gmail.com>
2024-12-06 08:56:14 -0300

    feat: set actions by subject

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,22 +2,15 @@ import {
   AbilityBuilder,
   CreateAbility,
   createMongoAbility,
-  ForcedSubject,
   MongoAbility,
 } from '@casl/ability'
 
 import { User } from './models/user'
 import { permissions } from './permissions'
+import { ProjectSubject } from './subjects/project'
+import { UserSubject } from './subjects/user'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
-export type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+export type AppAbilities = ProjectSubject | UserSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
